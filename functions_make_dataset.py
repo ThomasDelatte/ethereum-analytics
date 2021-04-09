99d09e5422a210832cb9f65fb6072b261d0e02b0
thomasdelatte <thomasdelatte@hotmail.com>
2021-04-09 13:24:28 +0200

    Reformatted with black.

--- functions_make_dataset.py
+++ functions_make_dataset.py
@@ -1,14 +1,19 @@
 import os
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] ="/home/tdelatte/new-projects/ethereum-analytics/key/ethereum-analytics-309308-6c01508bc0b8.json"
+
+os.environ[
+    "GOOGLE_APPLICATION_CREDENTIALS"
+] = "/home/tdelatte/new-projects/ethereum-analytics/key/ethereum-analytics-309308-6c01508bc0b8.json"
 
 import pandas as pd
 import pickle
 
+
 def load_data_from_bigquery(QUERY):
     """Loads data from BigQuery through GCP."""
-    query_job = client.query(QUERY) # API request
+    query_job = client.query(QUERY)  # API request
     df = query_job.to_dataframe()
     return df
+
 
 def add_mined_blocks(df):
     for i, row in df.iterrows():
